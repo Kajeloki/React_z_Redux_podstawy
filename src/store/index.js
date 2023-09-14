664ee996a56f46f5b4dec0e2a27819186f319589
Kajeloki <piotrkw200@wp.pl>
2023-09-14 22:13:46 +0200

    redux toolkit

--- src/store/index.js
+++ src/store/index.js
@@ -1,44 +1,12 @@
-import {createStore} from 'redux';
-const initialState = {counter: 0, showCounter: true}
-const counterReducer = (state=initialState, action) =>{
-    if(action.type==='inc'){
-        return {
-            counter: state.counter + 1,
-            showCounter: state.showCounter
-        };
-    };
-    if(action.type==='dec'){
-        return {
-            counter: state.counter - 1,
-            showCounter: state.showCounter
-        };
-    };
-    if(action.type==='ince'){
-        return {
-            counter: state.counter + action.amount,
-            showCounter: state.showCounter
-        };
-    };
-    if(action.type==='toogle'){
-        return {
-            counter: state.counter,
-            showCounter: !state.showCounter
-        };
-    };
-    
-        return state;
-    
-    
-};
+import { configureStore } from '@reduxjs/toolkit';
+import counterSlice from './counter';
+import authSlice from './auth';
 
-const counterSubscriber = ()=>{
-    const lastState = store.state;
-   // console.log(lastState);
-};
+const store = configureStore({
+    reducer: {counter: counterSlice.reducer,
+    auth: authSlice.reducer}
+});
 
-const store = createStore(counterReducer);
-store.subscribe(counterSubscriber)
-
-
-
+export  const counterActions = counterSlice.actions;
+export  const authActions = authSlice.actions;
 export default store;
